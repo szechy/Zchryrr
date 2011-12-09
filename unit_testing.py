81b7c20555877a3f40aaefb5bd4d5e9c79cfd88f
Zachary Orr <zachorr139@gmail.com>
2011-12-09 14:32:29 -0500

    New Unit Testing
    It's ballin'

--- unit_testing.py
+++ unit_testing.py
@@ -3,45 +3,51 @@ import tweepy
 import time
 from vars import *
 from zchryrr import Zchryrr, StreamListener
+from tweepy.error import *
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-
 timeout = 2
 zchryrr_test = Zchryrr()
-
 class TestZchryrr(unittest.TestCase):
 	#For testing Zchryrr
 	def setUp(self):
 		zchryrr_test = Zchryrr()
-
+		last_tweet = zchryrr_test.api.user_timeline()[0]
+	
+	def tearDown(self):
+		zchryrr_test = None
+				
 	def test_auth(self):
 		zchryrr_test.auth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
-		
-	'''def test_status_update(self):
-		zchryrr_test.api.update_status("@Zchryrr #STATUS")
-		time.sleep(timeout)
-		timeline = zchryrr_test.api.mentions()
-		last_tweet = timeline[0].text
-		print timeline[0].text
-		self.assertTrue(last_tweet == "@Zchryrr Zchryrr is currently online!")
-		print "" + timeline[0].id
-		print "" + timeline[1].id
-		print "" + timeline[0].text
-		print "" + timeline[1].text
-	'''
-	def test_mustache(self):
-		zchryrr_test.api.update_status("@Zchryrr #STACHE http://dl.dropbox.com/u/4132519/DSC_0091.JPG")
-		time.sleep(timeout)
-		timeline = zchryrr_test.api.mentions()
-		last_tweet = timeline[0].text
-		print timeline[0].text
-		self.assertTrue(last_tweet == "@Zchryrr http://mustachify.me/?src=http://dl.dropbox.com/u/4132519/DSC_0091.JPG")
-		print timeline
-	def test_prowl_ping(self):
-		zchryrr_test.send_prowl_ping()
 	
-			
+	def test_status_check(self):
+		try: tweet1 = zchryrr_test.api.update_status("@Zchryrr #STATUS")
+		except TweepError:
+			zchryrr_test.destroy_status("@Zchryrr #STATUS")
+			tweet1 = zchryrr_test.api.update_status("@Zchryrr #STATUS")
+		time.sleep(timeout)
+		zchryrr_test.api.destroy_status(tweet1.id)
+		mentions = zchryrr_test.api.mentions()
+		for tweet in mentions:
+			if("Zchryrr is currently" in tweet.text):
+				zchryrr_test.api.destroy_status(tweet.id)
+				break
+	
+	def test_mustache(self):
+		statuszor = "@Zchryrr #STACHE http://dl.dropbox.com/u/4132519/DSC_0091.JPG"
+		try: tweet2 = zchryrr_test.api.update_status(statuszor)
+		except TweepError:
+			zchryrr_test.delete_duplicate(statuszor)
+			tweet2 = zchryrr_test.api.update_status(statuszor)
+		time.sleep(timeout)
+		user_tweets = zchryrr_test.api.user_timeline()
+		zchryrr_test.api.destroy_status(tweet2.id)	
+		for tweet in user_tweets:
+			if("mustachify" in tweet.text or "t.co" in tweet.text):
+				zchryrr_test.api.destroy_status(tweet.id)	
+				break
+
 if __name__ == '__main__':
     unittest.main()
--- zchryrr.py
+++ zchryrr.py
@@ -1,5 +1,5 @@
 ### zchryrr.py
-### Version = 1.0.0
+### Version = 1.1.0
 
 import tweepy #Should be installed
 import string
@@ -10,6 +10,175 @@ import sys
 import urllib2
 import threading
 import pyrowl
+
+class Zchryrr:
+	def auth(self, consumer_key, consumer_secret, access_key, access_secret):
+		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_key, access_secret)
+		self.api = tweepy.API(auth)
+		self.stream = tweepy.Stream(auth, StreamListener(), timeout=None, secure=True)
+
+
+	def delete_duplicate(self, tweet):
+		user_tweets = self.api.user_timeline()
+		for tweets in user_tweets:
+			if(tweets.text.split(" ")[1] == tweet.split(" ")[1]):
+				zchryrr_test.api.destroy_status(tweets.id)
+				break
+			else:
+				pass
+
+
+	def get_status(self, user):
+		try:
+			self.api.update_status("@" + user + " Zchryrr is currently online!")
+		except:
+			error_handling("Running, but failed to send update")
+
+	def check_dms(self):
+		print "Checking DMs"
+		if self.api.direct_messages():
+			for message in self.api.direct_messages():
+				print "Found some messages"
+				print message.text
+				parse_objects_dm(message)
+				self.api.destroy_direct_message(message.id)
+		else:
+			print "I aint got no messages!"
+	
+		threading.Timer(60, self.check_dms).start()	
+
+	def mustache(self, url, user):
+		mustache_url = "http://mustachify.me/?src=" + url
+		self.api.update_status("@"+user+" "+mustache_url)
+
+	def follow_me(self, to_follow):
+		try:
+			self.api.create_friendship(split_command_array[2])
+			print "Following " + split_command_array[2]
+			error_handling("Now following " + split_command_array[2])
+		except:
+			error_handling("Failed to follow user " + split_command_array[2])
+	
+	def shutdown(self):
+		error_handling("Shutting down by request")
+		sys.exit(1)
+
+	def send_prowl_ping(self):
+		if(self.auth):
+			p.push("Zchryrr Ping", "", "Zchryrr was requested to ping you","","","")		
+		else:
+			print "Not authorized."
+			
+	def follow_back(self):
+		try:
+			followers = self.api.followers() #Gets an array (followers) of all of my followers
+		except:		
+			error_handling("Could not get followers")
+		try:
+			friends = self.api.friends() #Gets an array (friends) of all of the people I'm following
+		except:
+			error_handling("Could not get friends")
+			
+		followers_list = [u.screen_name for u in followers] #Sets followers so it's something readable
+		friends_list = [s.screen_name for s in friends] #sets friends so it's something readable
+		print followers_list #prints out followers
+		print friends_list #prints out following
+		for follower in followers_list:
+		    if follower not in friends_list:
+		    	tweepy.API(auth) #set's api
+		        to_follow.append(follower)	
+		        print to_follow
+		for follower in tweepy.Cursor(self.api.followers).items():
+			try:
+				if follower in tweepy.Cursor(self.api.friends).items():
+					print "Already following %s", follower.screen_name
+					pass
+				else:
+					print follower.screen_name
+					print follower in tweepy.Cursor(self.api.followers).items()
+					follower.follow()
+			except:
+				#error_handling("Couldn't follow back")
+				pass
+		pass
+		threading.Timer(3600, self.follow_back).start()
+
+zchryrr = Zchryrr()
+
+def error_handling(error_message):
+	if tweepy.error.TweepError:
+		p.push("@Zchryrr", "@Zchryrr is broken!", error_message)
+
+def parse_objects(message):
+	print "Message text: " + message.text
+	message_split = message.text.split(" ")
+	print message_split
+	array_count = len(message_split)
+	print array_count
+	if array_count > 1:
+		if message_split[0] == "@Zchryrr" or "@zchryrr":
+			check_command(message_split[1], message_split, message)
+			
+def parse_objects_dm(message):
+	print "Message text: " + message.text
+	message_split = message.text.split(" ")
+	print message_split
+	array_count = len(message_split)
+	print array_count
+	check_command(message_split[0], message_split, message)
+				
+def check_command(hashtag, split_command_array, original_object):
+	print hashtag
+	if hashtag == "#SHUTDOWN":
+		if original_object.author.screen_name in sudoers:
+			zchryrr.shutdown()
+		else:
+			try:
+				zchryrr.api.update_status(invalid_sudoer_message)
+			except:
+				zchryrr.delete_duplicate(invalid_sudoer_message)
+				zchryrr.api.update_status(invalid_sudoer_message)
+
+	if hashtag == "#PROWL":
+		zchryrr.send_prowl_ping()
+	
+	if hashtag == "#TOR":
+		try:
+			response = urllib2.urlopen(split_command_array[2]) # Some shortened url
+		except:
+			response = urllib2.urlopen(split_command_array[1]) # Some shortened url
+		url_destination = response.url
+		try:
+			os.system("cd " + torrent_directory + " && wget " + url_destination)
+			if split_command_array[1] == hashtag and original_object.author.screen_name in sudoers:
+				try:
+					error_handling(split_command_array[0] + " Mischief Manged >:3")
+				except:
+					error_handling("Running torrent but failed to update status")
+			else:
+				try:
+					zchryrr.api.update_status(invalid_sudoer_message)
+				except:
+					zchryrr.delete_duplicate(invalid_sudoer_message)
+					zchryrr.api.update_status(invalid_sudoer_message)
+		except:
+			error_handling("Torrenting " + url_destination + " failed")
+			
+	if hashtag == "#STATUS":
+		zchryrr.get_status(original_object.author.screen_name)
+
+	if hashtag == "#FOLLOW":
+		if original_object.author.screen_name in sudoers:
+			zchryrr.follow_me(split_command_array[2])
+		else:
+			error_handling(original_object.author.screen_name + " has tried to follow " + split_command_array[2]) 
+
+	if hashtag == "#STACHE":
+		try:
+			zchryrr.mustache(split_command_array[2], original_object.author.screen_name)
+		except:
+			error_handling("Could not mustache")
 
 class StreamListener(tweepy.StreamListener):
     def on_status(self, status):
@@ -34,155 +203,13 @@ class StreamListener(tweepy.StreamListener):
     	p.push("@Zchryrr", "Timeout", "@Zchryrr has timed out. Serious issue...","","","")		
         print 'Snoozing Zzzzzz'
 
-def error_handling(error_message):
-	if tweepy.error.TweepError:
-		p.push("@Zchryrr", "@Zchryrr is broken!", error_message)
-
-def check_dms():
-	print "Checking DMs"
-	if api.direct_messages():
-		for message in api.direct_messages():
-			print "Found some messages"
-			print message.text
-			parse_objects_dm(message)
-			api.destroy_direct_message(message.id)
-	else:
-		print "I aint got no messages!"
-	
-	threading.Timer(60, check_dms).start()	
-
-def parse_objects(message):
-	print "Message text: " + message.text
-	message_split = message.text.split(" ")
-	print message_split
-	array_count = len(message_split)
-	print array_count
-	if array_count > 1:
-		if message_split[0] == "@Zchryrr" or "@zchryrr":
-			check_command(message_split[1], message_split, message)
-			
-def parse_objects_dm(message):
-	print "Message text: " + message.text
-	message_split = message.text.split(" ")
-	print message_split
-	array_count = len(message_split)
-	print array_count
-	check_command(message_split[0], message_split, message)
-				
-def check_command(hashtag, split_command_array, original_object):
-	print hashtag
-	if hashtag == "#SHUTDOWN":
-		if original_object.author.screen_name == "ZacharyOrr":
-			shutitdown()
-			
-	if hashtag == "#PROWL":
-		send_prowl_ping()
-	
-	if hashtag == "#TOR":
-		try:
-			response = urllib2.urlopen(split_command_array[2]) # Some shortened url
-		except:
-			response = urllib2.urlopen(split_command_array[1]) # Some shortened url
-		url_destination = response.url
-		try:
-			os.system("cd " + torrent_directory + " && wget " + url_destination)
-			if split_command_array[1] == hashtag and original_object.author.screen_name in authorized_torrenters:
-				try:
-					error_handling(split_command_array[0] + " Mischief Manged >:3")
-				except:
-					error_handling("Running torrent but failed to update status")
-			else:
-				error_handling(original_object.author.screen_name + " has attempted to start a torrent.")
-		except:
-			error_handling("Torrenting " + url_destination + " failed")
-			
-	if hashtag == "#STATUS":
-		get_status(original_object.author.screen_name)
-
-	if hashtag == "#FOLLOW":
-		if original_object.author.screen_name == "ZacharyOrr":
-			functions.follow_me(split_command_array[2])
-		else:
-			error_handling(original_object.author.screen_name + " has tried to follow " + split_command_array[2]) 
-
-	if hashtag == "#STACHE":
-		try:
-			functions.mustache(split_command_array[2], original_object.author.screen_name)
-		except:
-			error_handling("Could not mustache")
-
-#Functions
-def mustache(url, user):
-	mustache_url = "http://mustachify.me/?src=" + url
-	api.update_status("@"+user+" "+mustache_url)
-
-def follow_me(to_follow):
-	try:
-		api.create_friendship(split_command_array[2])
-		print "Following " + split_command_array[2]
-		error_handling("Now following " + split_command_array[2])
-	except:
-		error_handling("Failed to follow user " + split_command_array[2])
-
-def get_status(user):
-	try:
-		api.update_status("@" + user + " Zchryrr is currently online!")
-	except:
-		error_handling("Running, but failed to send update")
-
-def shutitdown():
-	error_handling("Shutting down by request")
-	sys.exit(1)
-
-def send_prowl_ping():
-	p.push("Twitter DM", "DM'd", "Zchryrr was requested to DM you","","","")		
-
-def follow_back():
-	try:
-		followers = api.followers() #Gets an array (followers) of all of my followers
-	except:		
-		error_handling("Could not get followers")
-	try:
-		friends = api.friends() #Gets an array (friends) of all of the people I'm following
-	except:
-		error_handling("Could not get friends")
-		
-	followers_list = [u.screen_name for u in followers] #Sets followers so it's something readable
-	friends_list = [s.screen_name for s in friends] #sets friends so it's something readable
-	print followers_list #prints out followers
-	print friends_list #prints out following
-	for follower in followers_list:
-	    if follower not in friends_list:
-	    	tweepy.API(auth) #set's api
-	        to_follow.append(follower)	
-	        print to_follow
-	for follower in tweepy.Cursor(api.followers).items():
-		try:
-			if follower in tweepy.Cursor(api.friends).items():
-				print "Already following %s", follower.screen_name
-				pass
-			else:
-				print follower.screen_name
-				print follower in tweepy.Cursor(api.followers).items()
-				follower.follow()
-		except:
-			#error_handling("Couldn't follow back")
-			pass
-	pass
-	threading.Timer(3600, follow_back).start()
-
 def main():
+	zchryrr.auth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 	p.push("@Zchryrr", "Status", "@Zchryrr has started!", "", "", "")
 	#follow_back()
 	while 1:
-		stream.userstream()
+		zchryrr.stream.userstream()
 		
-#Tweepy API Stuff
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
-stream = tweepy.Stream(auth, StreamListener(), timeout=None, secure=True)
-
 if __name__ == '__main__':
     try:
         main()
