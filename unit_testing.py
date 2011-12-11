52f686dc16ff6db68947c6ac4bfb16e018a19ed6
Zachary Orr <zachorr139@gmail.com>
2011-12-11 17:05:10 -0500

    Revamp'd Unit Testing

--- unit_testing.py
+++ unit_testing.py
@@ -8,7 +8,7 @@ from tweepy.error import *
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-timeout = 2
+timeout = 1
 zchryrr_test = Zchryrr()
 class TestZchryrr(unittest.TestCase):
 	#For testing Zchryrr
@@ -22,12 +22,8 @@ class TestZchryrr(unittest.TestCase):
 		zchryrr_test.auth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 	
 	def test_status_check(self):
-		try: tweet1 = zchryrr_test.api.update_status("@Zchryrr #STATUS")
-		except TweepError:
-			zchryrr_test.destroy_status("@Zchryrr #STATUS")
-			tweet1 = zchryrr_test.api.update_status("@Zchryrr #STATUS")
+		zchryrr_test.get_status("Zchryrr")
 		time.sleep(timeout)
-		zchryrr_test.api.destroy_status(tweet1.id)
 		mentions = zchryrr_test.api.mentions()
 		for tweet in mentions:
 			if("Zchryrr is currently" in tweet.text):
@@ -35,15 +31,10 @@ class TestZchryrr(unittest.TestCase):
 				break
 	
 	def test_mustache(self):
-		statuszor = "@Zchryrr #STACHE http://dl.dropbox.com/u/4132519/DSC_0091.JPG"
-		try: tweet2 = zchryrr_test.api.update_status(statuszor)
-		except TweepError:
-			zchryrr_test.delete_duplicate(statuszor)
-			tweet2 = zchryrr_test.api.update_status(statuszor)
+		zchryrr_test.mustache("http://dl.dropbox.com/u/4132519/DSC_0091.JPG", "Zchryrr")
 		time.sleep(timeout)
-		user_tweets = zchryrr_test.api.user_timeline()
-		zchryrr_test.api.destroy_status(tweet2.id)	
-		for tweet in user_tweets:
+		mentions = zchryrr_test.api.mentions()
+		for tweet in mentions:
 			if("mustachify" in tweet.text or "t.co" in tweet.text):
 				zchryrr_test.api.destroy_status(tweet.id)	
 				break
--- zchryrr.py
+++ zchryrr.py
@@ -10,6 +10,7 @@ import sys
 import urllib2
 import threading
 import pyrowl
+import inspect_shell
 
 class Zchryrr:
 	def auth(self, consumer_key, consumer_secret, access_key, access_secret):
